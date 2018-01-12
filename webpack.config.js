3ded553030453c0f02d68f8d173f168d1c431cf4
sihor <sihorjs@gmail.com>
2018-01-12 12:03:48 +0200

    initial config

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+	
+	HtmlWebpackPlugin = require("html-webpack-plugin");
+	ExtractTextPlugin = require("extract-text-webpack-plugin");
+	webpack = require('webpack');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src', 'index.js'),
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				include: path.resolve(__dirname, 'src'),
+				use: 'babel-loader'
+			},
+			{
+            	test: /\.less$/,
+	            use: ExtractTextPlugin.extract({
+	                use: [
+	                	{loader: "css-loader"}, 
+	                	{loader: "less-loader"}
+	                ],
+	                fallback: "style-loader"
+	            })
+        	},
+        	{
+        		test: /\.(png|svg|jpg|gif)$/,
+        		use: 'file-loader'
+        	},
+        	{
+        		test: /\.(woff|woff2|eot|ttf|otf)$/,
+        		use: 'file-loader'
+        	}
+		]	
+	},
+	plugins: [
+    	new HtmlWebpackPlugin({
+      		template: path.resolve(__dirname, 'public', 'index.html'),
+    	}),
+    	new ExtractTextPlugin("style.css"),
+    	new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+  	],
+  	devServer: {
+  		port: 9000,
+  		contentBase: path.resolve(__dirname, 'public'),
+  		hot: true
+	}
+};
