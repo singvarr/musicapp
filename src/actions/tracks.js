22dbc40f5b3c473495388f2e14732c3949dfe22c
sihor <sihorjs@gmail.com>
2018-01-13 20:49:21 +0200

    add action types and action creators

---
+++ src/actions/tracks.js
@@ -0,0 +1,36 @@
+export const REQUEST_TRACKS = "REQUEST_TRACKS";
+export const GET_TOP_TRACKS_SUCCESS = "GET_TOP_TRACKS_SUCCESS";
+export const GET_TOP_TRACKS_ERROR = "GET_TOP_TRACKS_ERROR";
+
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+const TOP_TRACKS_URL = "https://rss.itunes.apple.com/api/v1/ua/apple-music/top-songs/all/100/explicit.json";
+
+export function requestTracks() {
+	return {
+		type: REQUEST_TRACKS
+	}
+}
+
+export const getTopTracks = () => dispatch => {
+	dispatch(requestTracks());
+	return fetch(PROXY_URL + TOP_TRACKS_URL, {mode: "cors"})
+		.then(res => res.json())
+		.then(data => data.feed.results)
+		.then(tracks => dispatch(getTopTracksSuccess(tracks)))
+		.then(() => dispatch(requestTracks()))
+		.catch(error => getTopTracksError(error))
+}
+
+export function getTopTracksSuccess(payload) {
+	return {
+		type: GET_TOP_TRACKS_SUCCESS,
+		payload
+	}
+}
+
+export function getTopTracksError(error) {
+	return {
+		type: GET_TOP_TRACKS_ERROR,
+		error
+	}
+}
