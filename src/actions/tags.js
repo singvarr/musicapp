14b90b14ee398633f6bc3caa7dbbb43b6398235a
sihor <sihorjs@gmail.com>
2018-01-17 16:04:12 +0200

    add actions for deleting genre filters

--- src/actions/tags.js
+++ src/actions/tags.js
@@ -1,8 +1,23 @@
 export const FILTER_TRACKS_BY_TAGS = "FILTER_TRACKS_BY_TAG";
+export const DELETE_TAG = "DELETE_TAG";
+export const DELETE_ALL_TAGS = "DELETE_ALL_TAGS";
 
 export function filterTracksByTags(tag) {
 	return {
 		type: FILTER_TRACKS_BY_TAGS,
 		tag
+	}
+}
+
+export function deleteTag(tag) {
+	return {
+		type: DELETE_TAG,
+		tag
+	}
+}
+
+export function deleteAllTags() {
+	return {
+		type: DELETE_ALL_TAGS
 	}
 }
