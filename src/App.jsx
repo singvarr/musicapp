264967b6001df1c2c8eee7b966c3a622e4ce7fec
sihor <sihorjs@gmail.com>
2018-01-20 19:45:44 +0200

    add routes, move all app structure to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import React, { Fragment } from "react";
 import Header from "./components/header.jsx";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch, hashHistory } from "react-router-dom";
+import store from "./store/index.js";
+
 import TracksContainer from "./containers/TracksContainer";
+import Main from "./components/main.jsx";
+import TrackContainer from "./containers/TrackContainer";
 
 function App() {
 	return (
-		<Fragment>
-			<Header/>
-			<TracksContainer/>
-		</Fragment>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Fragment>
+					<Header/>
+					<main>
+					<Switch>
+						<Route path="/" exact component={Main}/>
+						<Route path="/tracks" exact component={TracksContainer}/>
+						<Route path="/tracks/:id" component={TrackContainer}/>
+					</Switch>
+					</main>
+				</Fragment>
+			</BrowserRouter>
+		</Provider>
 	)
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./store/index.js";
-
-import "./less/index.less";
+import { render } from "react-dom";
 
 import App from "./App.jsx";
 
-ReactDOM.render(<Provider store={store}><App/></Provider>,
-	document.getElementById("root"))
+import "./less/index.less";
+
+render(<App/>,document.getElementById("root"))
