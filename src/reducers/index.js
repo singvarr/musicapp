6158c7ee27056e876aa921ff29e75b140bbef6d6
sihor <sihorjs@gmail.com>
2018-01-21 20:16:27 +0200

    add reducer for router

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 
+import { routerReducer } from "react-router-redux";
 import { getTopTracks } from "./tracks";
 import { requestTracks } from "./fetching";
 import filterTracks from "./filter";
 
 const reducer = combineReducers({
+	route: routerReducer,
 	trackList: getTopTracks,
 	isFetching: requestTracks,
 	tags: filterTracks
