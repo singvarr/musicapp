116c4c6621967cb785442b70e73dae9adda9f7bc
sihor <sihorjs@gmail.com>
2018-01-21 12:36:17 +0200

    put navigation component in header, change src of image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import Navigation from "./navigation.jsx";
+
 function Header() {
 	return (
 		<header>
-			<div className="logo"><img src="./assets/img/logo.png" alt="logo"/>New Radio</div>
+			<div className="logo"><img src="/assets/img/logo.png" alt="logo"/>New Radio</div>
+			<Navigation/>
 			<a className="donate"
 				href="https://www.liqpay.ua/ru/checkout/mynewtestradio" target="_blank">Donate</a>
 		</header>
