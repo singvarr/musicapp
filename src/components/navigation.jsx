b41525aad3b4fac26c9cf389d86746225b817587
sihor <sihorjs@gmail.com>
2018-01-22 11:45:58 +0200

    add navigation component

---
+++ src/components/navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavLink } from "react-router-dom"
+
+function Navigation() {
+	return (
+		<nav className="navigation">
+			<ul>
+				<li><NavLink to="/" exact>Main</NavLink></li>
+				<li><NavLink to="/tracks">Tracks</NavLink></li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navigation;
