396b7c86d2fa5e24058640b05c67c6dc7cdbcc6c
sihorjs <sihorjs@gmail.com>
2018-09-02 12:02:27 +0300

    delete common config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,29 @@
-const path = require("path")
-	webpack = require('webpack');
-	merge = require('webpack-merge');
-	common = require('./webpack.common.js');
+const path = require("path");
+webpack = require("webpack");
 
-module.exports = merge(common, {
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-      			use: "eslint-loader"
-			},
-			{
-				test: /\.less$/,
-	        	use: ["style-loader", "css-loader", "less-loader"]
-	       	}
-		]
-	},
-	plugins: [	
-		new webpack.NamedModulesPlugin(),
-		new webpack.HotModuleReplacementPlugin()
-	],
-	devServer: {
-  		port: 9000,
-  		historyApiFallback: true,
-  		contentBase: path.resolve(__dirname, 'public'),
-  		hot: true
-	}
-});
+module.exports = {
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "eslint-loader"
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    port: 9000,
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "public"),
+    hot: true
+  }
+};
