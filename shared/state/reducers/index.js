cb2c47c540c2a14be0588ac01fc1e66fb242f9c0
sihorjs <sihorjs@gmail.com>
2018-09-11 13:07:30 +0300

    delete router reducer

--- shared/state/reducers/index.js
+++ shared/state/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
-import { routerReducer } from "react-router-redux";
 
 import { getTopTracks } from "./tracks";
 import { requestTracks } from "./fetching";
 import filterTracks from "./filter";
 
 const reducer = combineReducers({
-    route: routerReducer,
     trackList: getTopTracks,
     isFetching: requestTracks,
     tags: filterTracks,
