2f2735a3224edb87d43ca524ce05b3b372cfdde4
sihorjs <sihorjs@gmail.com>
2018-09-11 13:22:38 +0300

    fix src path of img, detete "target=_blank" of link

--- shared/components/Header.jsx
+++ shared/components/Header.jsx
@@ -6,14 +6,13 @@ function Header() {
     return (
         <header>
             <div className="logo">
-                <img src="/assets/img/logo.png" alt="logo" />
+                <img src="img/logo.png" alt="logo" />
                 New Radio
             </div>
             <Navigation />
             <a
                 className="donate"
                 href="https://www.liqpay.ua/ru/checkout/mynewtestradio"
-                target="_blank"
             >
                 Donate
             </a>
