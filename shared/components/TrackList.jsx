214dba1ad281ac1c47bb7a10574fb9d03c00c72d
sihorjs <sihorjs@gmail.com>
2018-09-12 11:24:43 +0300

    fetch data on mount

--- shared/components/TrackList.jsx
+++ shared/components/TrackList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -7,61 +7,63 @@ import TagFilter from "./TagFilter";
 import { getTopTracks, filterTracksByTags } from "../state/actions";
 import { getFilteredTracks } from "../state/filters";
 
-function TrackList(props) {
-    return (
-        <section className="tracks">
-            <div className="search-tracks">
-                Look for good music? Explore our top 100!
-                <button onClick={props.onGetTopTracks}>Get it!</button>
-            </div>
-            <TagFilter />
-            {props.isFetching ? (
-                <div className="loading">Loading...</div>
-            ) : (
-                <div className="tracks-list">
-                    {props.tracks.map((item, i) => {
-                        return (
-                            <div className="track" key={i}>
-                                <Link to={`tracks/${item.artistId}`}>
-                                    <div className="track-img">
-                                        <img src={item.artworkUrl100} />
+class TrackList extends Component {
+    componentDidMount() {
+        this.props.onGetTopTracks();
+    }
+
+    render() {
+        return (
+            <section className="tracks">
+                <TagFilter />
+                {this.props.isFetching ? (
+                    <div className="loading">Loading...</div>
+                ) : (
+                    <div className="tracks-list">
+                        {this.props.tracks.map((item, i) => {
+                            return (
+                                <div className="track" key={i}>
+                                    <Link to={`/tracks/${item.artistId}`}>
+                                        <div className="track-img">
+                                            <img src={item.artworkUrl100} />
+                                        </div>
+                                    </Link>
+                                    <div className="track-info">
+                                        <div className="title">
+                                            {i + 1}. {item.name}
+                                        </div>
+                                        <div className="performer">
+                                            {item.artistName}
+                                        </div>
+                                        <ul className="tags">
+                                            {item.genres.map((genre, i) => (
+                                                <li
+                                                    key={i}
+                                                    onClick={() =>
+                                                        this.props.onAddTagInFilter(
+                                                            genre.name
+                                                        )
+                                                    }
+                                                    className="tag"
+                                                >
+                                                    {genre.name}
+                                                </li>
+                                            ))}
+                                        </ul>
                                     </div>
-                                </Link>
-                                <div className="track-info">
-                                    <div className="title">
-                                        {i + 1}. {item.name}
-                                    </div>
-                                    <div className="performer">
-                                        {item.artistName}
-                                    </div>
-                                    <ul className="tags">
-                                        {item.genres.map((genre, i) => (
-                                            <li
-                                                key={i}
-                                                onClick={() =>
-                                                    props.onAddTagInFilter(
-                                                        genre.name
-                                                    )
-                                                }
-                                                className="tag"
-                                            >
-                                                {genre.name}
-                                            </li>
-                                        ))}
-                                    </ul>
                                 </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            )}
-            {props.hasError && (
-                <div className="error">
-                    Sorry, error occured. Please, try again later
-                </div>
-            )}
-        </section>
-    );
+                            );
+                        })}
+                    </div>
+                )}
+                {this.props.hasError && (
+                    <div className="error">
+                        Sorry, error occured. Please, try again later
+                    </div>
+                )}
+            </section>
+        );
+    }
 }
 
 function mapStateToProps(state) {
