6a9af42edfd5f4b92818520a81a7dee981719484
sihorjs <sihorjs@gmail.com>
2018-09-12 11:24:21 +0300

    move creation of store to main client file

--- client/index.js
+++ client/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { hydrate } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import thunk from "redux-thunk";
 
 import App from "../shared/components/App";
-import store from "../shared/state/store";
+import reducer from "../shared/state/reducers";
+
 import "../shared/less/index.less";
+
+const preloadedState = window.__PRELOADED_STATE__;
+delete window.__PRELOADED_STATE__;
+
+const middlewares = [thunk];
+const store = createStore(
+    reducer,
+    preloadedState,
+    applyMiddleware(...middlewares)
+);
 
 hydrate(
     <Provider store={store}>
--- shared/state/store/index.js
+++ shared/state/store/index.js
@@ -1,9 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-
-import reducer from "../reducers";
-
-export const initialState = {
+const initialState = {
     trackList: {
         tracks: [],
         hasError: false,
@@ -12,12 +7,4 @@ export const initialState = {
     tags: [],
 };
 
-const middlewares = [thunk];
-
-const store = createStore(
-    reducer,
-    initialState,
-    applyMiddleware(...middlewares)
-);
-
-export default store;
+export default initialState;
