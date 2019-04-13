da856b5c663921b14461e18ad87132a1e1cdda3c
sihor <sihorjs@gmail.com>
2019-04-13 13:25:06 +0300

    prettify components, add types

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navigation from "components/Navigation";
+
+function Header() {
+    return (
+        <header>
+            <div className="logo">
+                <img src="/assets/img/logo.png" alt="logo" />
+                New Radio
+            </div>
+            <Navigation />
+            <a
+                className="donate"
+                href="https://www.liqpay.ua/ru/checkout/mynewtestradio"
+                target="_blank"
+            >
+                Donate
+            </a>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/types/track.d.ts
@@ -0,0 +1,11 @@
+type TrackType = {
+    artistId: string;
+    artistName: string;
+    artworkUrl100: string;
+    collectionName: string;
+    genres: { name: string }[];
+    name: string;
+    releaseDate: string;
+};
+
+export default TrackType;
