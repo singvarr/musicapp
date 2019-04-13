3a83ff524cee4924c65576d0038aa8d4b688516c
sihor <sihorjs@gmail.com>
2019-04-13 13:40:15 +0300

    update imports in redux files, delete obsolete reducer

--- src/store/reducers/filter.js
+++ src/store/reducers/filter.js
@@ -1,18 +1,20 @@
-import { 
-	FILTER_TRACKS_BY_TAGS,
-	DELETE_TAG,
-	DELETE_ALL_TAGS
- } from "../actions";
+import {
+    FILTER_TRACKS_BY_TAGS,
+    DELETE_TAG,
+    DELETE_ALL_TAGS
+} from "actions/tags";
 
 function filterTracks(state = [], action) {
-	switch(action.type) {
-		case FILTER_TRACKS_BY_TAGS: 
-			return [...new Set(state).add(action.tag)];
-		case DELETE_TAG:
-			return state.filter(tag => tag != action.tag);
-		case DELETE_ALL_TAGS: return [];
-		default: return state;
-	}
+    switch (action.type) {
+    case FILTER_TRACKS_BY_TAGS:
+        return [...new Set(state).add(action.tag)];
+    case DELETE_TAG:
+        return state.filter(tag => tag != action.tag);
+    case DELETE_ALL_TAGS:
+        return [];
+    default:
+        return state;
+    }
 }
 
 export default filterTracks;
