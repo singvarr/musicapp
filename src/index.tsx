95853e1e025cb9533a3487abffb242e765edfe46
sihor <sihorjs@gmail.com>
2019-04-19 21:00:41 +0300

    add hot reload

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import App from "components/App";
 
-render(<App />, document.getElementById("root"));
+function renderApp(): void {
+    render(<App />, document.getElementById("root"));
+}
+
+renderApp();
+
+if (module.hot) {
+    module.hot.accept((): void => renderApp());
+}
