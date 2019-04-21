de7f01d575029c065eb729af28bfe567524e184d
sihor <sihorjs@gmail.com>
2019-04-21 11:34:55 +0300

    add fixture for main menu

---
+++ src/fixtures/navigation.ts
@@ -0,0 +1,14 @@
+const menu = [
+    {
+        exact: true,
+        linkTo: "/",
+        name: "Home"
+    },
+    {
+        exact: false,
+        linkTo: "/tracks",
+        name: "Tracks"
+    }
+];
+
+export default menu;
