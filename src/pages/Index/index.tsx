1461d7b7f0ffe5b0013846a8d5db8698a596d322
Igor Sokolov <you@example.com>
2019-05-10 12:04:50 +0300

    rename home to index page

---
+++ src/pages/Index/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from "layouts/MainLayout";
+
+function Home(): JSX.Element {
+    return (
+        <MainLayout>
+            <section>This is home page</section>
+        </MainLayout>
+    );
+}
+
+export default Home;
