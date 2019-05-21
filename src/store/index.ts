65cb12e017a0fadacdcccfd2cd50a3478ac7fd71
Igor Sokolov <you@example.com>
2019-05-21 16:18:52 +0300

    add redux-devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,17 @@
 import { createStore, combineReducers, applyMiddleware, Store } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import tracks from "store/tracks";
 import State from "types/state";
 
-export const reducer = combineReducers({ tracks });
+const { NODE_ENV } = process.env;
 
-const opts = { debug: true };
-export const makeStore = (initialState: State, options = opts): Store =>
-    createStore(reducer, initialState, applyMiddleware(thunk));
+const middlewares = applyMiddleware(thunk);
+const enhancer =
+    NODE_ENV === "development" ? composeWithDevTools(middlewares) : middlewares;
+
+export const reducer = combineReducers({ tracks });
+const makeStore = (initialState: State): Store =>
+    createStore(reducer, initialState, enhancer);
 
 export default makeStore;
